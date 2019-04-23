0f8d97b5d7628dc5260475198883006ce0d2f5c8
Brett Jory <bjory7@gmail.com>
2019-04-23 09:07:46 -0700

    completed features: user registration, login, logout. Navbar displays dynamic content according to login status

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('register', views.reg_form),
-    path('register/process', views.reg_process)
+    path('register/process', views.reg_process),
+    path('login', views.login_form),
+    path('login/process', views.login_process),
+    path('logout', views.logout_process),
+    path('profile/view/<int:userid>', views.profile_view)
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from .models import *
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -9,7 +10,37 @@ def reg_form(request):
     return render(request, "users/regform.html")
 
 def reg_process(request):
-    #if fail
-    return redirect('/register')
-    #else
-    return redirect('/dashboard')
+    if not User.objects.is_original("email", request.POST["email"]):
+        messages.error(request, "User already exists")
+        return redirect("/register")
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/register")
+    new_user = User.objects.add_user(request.POST)
+    request.session["userid"] = new_user.id
+    messages.success(request, "Successfully registered!")
+    return redirect("/dashboard")
+
+def login_form(request):
+    if "userid" in request.session:
+        messages.error(request, "Already logged in!")
+        return redirect("/")
+    return render(request, "users/loginform.html")
+
+def login_process(request):
+    if User.objects.login(request.POST):
+        request.session["userid"] = User.objects.get_id(request.POST)
+        messages.success(request, "Successfully logged in!")
+        return redirect("/dashboard")
+    messages.error(request, "You could not be logged in")
+    return redirect("/login")
+
+def logout_process(request):
+    request.session.clear()
+    messages.success(request, "Logged out")
+    return redirect("/")
+
+def profile_view(request, userid):
+    return render("users/profilepage.html")
